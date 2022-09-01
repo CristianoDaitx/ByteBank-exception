5c0052f9557409c4aa441b5dd12db3adb1cfc5cb
CristianoDaitx <cristiano.daitx@gmail.com>
2022-08-31 22:49:25 -0300

    Criado a excecao SaldoInsuficiente, atualizado o program.cs para testar a nova exception.

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,6 +10,31 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(5025, 52665);
+                conta.Depositar(50);
+                Console.WriteLine(conta.Saldo);
+                conta.Sacar(500);
+                Console.WriteLine(conta.Saldo);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Erro no parâmetro: " + ex.ParamName);
+                Console.WriteLine("Ocorreu um erro do tipo ArgumentException");
+                Console.WriteLine(ex.Message);
+            }
+            catch (SaldoInsuficienteException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Exceção do tipo SaldoInsuficienteException");
+            }
+
+
+
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+
             Console.ReadLine();
         }
     }
